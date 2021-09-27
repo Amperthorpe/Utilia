e63d828b05ab5d0ca59124f12bc3ab822693c333
Alleluid <milotig@gmail.com>
2021-09-26 21:55:30 -0700

    Implement Bottled Bedrock. Implement crafting for several items. Add item textures for boots, player models NYI.

--- src/main/java/net/latenighters/utilia/Registration.java
+++ src/main/java/net/latenighters/utilia/Registration.java
@@ -5,10 +5,7 @@ import net.latenighters.utilia.common.blocks.BlockPillow;
 import net.latenighters.utilia.common.blocks.suppressor.BlockSuppressor;
 import net.latenighters.utilia.common.blocks.suppressor.TileSuppressor;
 import net.latenighters.utilia.common.entities.LightArrowEntity;
-import net.latenighters.utilia.common.items.ChalkItem;
-import net.latenighters.utilia.common.items.ItemLightArrow;
-import net.latenighters.utilia.common.items.LongFallBoots;
-import net.latenighters.utilia.common.items.SpikeBoots;
+import net.latenighters.utilia.common.items.*;
 import net.minecraft.block.Block;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityClassification;
@@ -50,6 +47,7 @@ public class Registration {
     public static final RegistryObject<ItemLightArrow> LIGHT_ARROW = ITEMS.register("light_arrow", ItemLightArrow::new);
     public static final RegistryObject<LongFallBoots> LONG_FALL_BOOTS = ITEMS.register("long_fall_boots", LongFallBoots::new);
     public static final RegistryObject<SpikeBoots> SPIKE_BOOTS = ITEMS.register("spike_boots", SpikeBoots::new);
+    public static final RegistryObject<ItemBottledBedrock> BOTTLED_BEDROCK = ITEMS.register("bottled_bedrock", ItemBottledBedrock::new);
 
     // Block Registration
     public static final RegistryObject<BlockSuppressor> SUPPRESSOR_BLOCK = BLOCKS.register("suppressor", BlockSuppressor::new);
--- src/main/java/net/latenighters/utilia/common/EventSubscriber.java
+++ src/main/java/net/latenighters/utilia/common/EventSubscriber.java
@@ -1,12 +1,17 @@
 package net.latenighters.utilia.common;
 
 import net.latenighters.utilia.common.blocks.suppressor.TileSuppressor;
+import net.latenighters.utilia.common.items.ItemBottledBedrock;
 import net.latenighters.utilia.common.items.SpikeBoots;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
+import net.minecraftforge.event.entity.item.ItemEvent;
+import net.minecraftforge.event.entity.item.ItemTossEvent;
+import net.minecraftforge.event.entity.living.LivingEntityUseItemEvent;
 import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.event.entity.living.LivingFallEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.eventbus.EventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -22,5 +27,15 @@ public class EventSubscriber { // Style: Keep logic in item/block classes when p
     @SubscribeEvent
     public static void onEvent(LivingFallEvent event) {
         SpikeBoots.handleLivingFallEvent(event);
+    }
+
+    @SubscribeEvent
+    public static void onEvent(PlayerInteractEvent.RightClickBlock event) {
+        ItemBottledBedrock.onPlayerRightClickBlockEvent(event);
+    }
+
+    @SubscribeEvent
+    public static void onEvent(ItemTossEvent event) {
+
     }
 }
